c8e999b0b6c57c4d4859d9e333ccfd4aa650218c
kevin duan <agonyice0115@gmail.com>
2016-12-14 16:11:21 +0800

    init project

---
+++ 1day.js
@@ -0,0 +1,12 @@
+/**
+ * Created by kevin on 14/12/2016.
+ *
+ */
+var x = 3;
+var y = 1.1;
+console.log(`x+y ${x + y}`);
+console.log(`x-y ${x - y}`);
+console.log(`x*y ${x * y}`);
+console.log(`x/y ${x / y}`);
+console.log(`x%y ${x % y}`);
+
